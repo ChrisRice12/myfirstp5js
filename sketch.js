f895f622b9b5198b14edbe9fb27de25c664e84cb
Christian Rice <ctr22b@fsu.edu>
2023-09-12 23:37:06 -0400

    myfirstp5js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER)
+  fill("red")
+  noStroke()
 }
 
 function draw() {
-  background(125);
+  background("RebeccaPurple" );
+
+fill("GhostWhite");
+  rect(200, 250, 165, 165,);
+
+  fill("Salmon")
+  rect(200, 277.5, 135, 135,);
+
+  fill('blue')
+  rect(200, 300, 100, 100,);
+
+  
+
+  
+
+
+ fill("PaleGoldenRod");
+rect(100, 200, 20, 300)
+  
+  fill("PaleGoldenRod");
+  rect(300, 200, 20, 300);
+
+  fill("PaleGoldenRod");
+  rect(150, 160, 15, 100,);
+
+  fill ("PaleGoldenRod");
+  rect (250, 160, 15, 100);
+
+  fill ("PaleGoldenRod");
+  rect(180, 270, 10, 40);
+
+  fill ("PaleGoldenRod");
+  rect(220, 270, 10, 40)
+
+  fill ("PaleGoldenRod");
+  rect(200, 255, 100, 10)
+
+
 }
